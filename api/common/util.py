50c10db5641f2cccec6db63e334ee1b2ae35a96d
mezklador <s.esteve@mezklador.com>
2016-03-25 16:22:05 +0100

    Vault() is dead...

--- api/common/util.py
+++ api/common/util.py
@@ -1,36 +1,23 @@
-import uuid
-import bcrypt
-import moment
 from base64 import b64encode, b64decode
+from Crypto.Cipher import AES
+import os
+import uuid
 
-class Vault:
-    def __init__(self, value=None):
-        if value is not None:
-            self.target = value
-        else:
-            self.target = moment.utcnow().timezone('Europe/Paris').format('YYYY-M-D H:m').encode('utf-8')
-        self.bcrypt_value = None
-        self.b64_value = None
+def rndKey(lg=32):
+    if lg >= 32:
+        return uuid.uuid4().hex
+    return uuid.uuid4().hex[-lg:]
 
-    def encrypt(self, length=10):
-        self.bcrypt_value = bcrypt.hashpw(self.target, bcrypt.gensalt(length))
-        self.b64_value = b64encode(self.bcrypt_value)
-        return self.show()
+BLOCK_SIZE = 32
+PADDING = '#'
 
-    def show(self):
-        return self.b64_value.decode()
+def _pad(data, pad_with=PADDING):
+    return data + (BLOCK_SIZE - len(data) % BLOCK_SIZE) * PADDING
 
-    def is_valid(self, req_value):
-        req_value = b64decode(req_value.encode('ascii'))
-        # print("entry: {}\nIndoor: {}".format(req_value, self.bcrypt_value))
-        return req_value == self.bcrypt_value
+def encryption(secret_key, data):
+    cipher = AES.new(_pad(secret_key, '@')[:32])
+    return b64encode(cipher.encrypt(_pad(data)))
 
-    def invalid(self, req_value):
-        return not self.is_valid(req_value)
-
-class RndKey:
-    def __init__(self):
-        self.key = uuid.uuid4().hex
-    
-    def get(self):
-        return self.key
+def decryption(secret_key, encrypted_data):
+    cipher = AES.new(_pad(secret_key, '@')[:32])
+    return cipher.decrypt(b64decode(encrypted_data)).rstrip(PADDING)
